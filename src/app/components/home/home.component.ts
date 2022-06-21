2af8df206b364bec5b586fca63edcb9b7c69ec51
chinds <chris.hinds@istation.com>
2022-06-21 15:05:28 -0500

    removed ref to translate service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-    constructor(private translateService: TranslateService) {
-        translateService.setDefaultLang('en')
-    }
-
-    ngOnInit(): void {
-        console.log(this.translateService.currentLang)
-    }
-
-}
+export class HomeComponent { }
