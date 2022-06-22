3337fe83f30f70c45a2cc3fed9b7722e05a4904f
ghchfolio <ghchfolio@gmail.com>
2022-06-21 19:46:10 -0500

    added tranlsated content for about pg

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MissingTranslationHandler, TranslateModule } from '@ngx-translate/core';
 import { AboutComponent } from './about.component';
 
 const routes: Routes = [{ path: '', component: AboutComponent }];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [
+        RouterModule.forChild(routes),
+        TranslateModule.forChild({ isolate: false })
+    ],
     exports: [RouterModule],
     declarations: [
         AboutComponent
     ]
 })
 export class AboutRoutingModule { }
+// TODO add MissingTranslationHandler
