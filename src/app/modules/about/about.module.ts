6e470943a04c8f3d5e644f4f847c4cf7f0a4fdf8
ghchfolio <ghchfolio@gmail.com>
2022-06-26 21:05:10 -0500

    removed missing translations module

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MissingTranslationHandler, TranslateModule } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { AboutComponent } from './about.component';
 
 const routes: Routes = [{ path: '', component: AboutComponent }];
 
 @NgModule({
-   imports: [
-      RouterModule.forChild(routes),
-      TranslateModule.forChild({ isolate: false })
-   ],
-   exports: [RouterModule],
-   declarations: [
-      AboutComponent
-   ]
+    imports: [
+        RouterModule.forChild(routes),
+        TranslateModule.forChild({ isolate: false })
+    ],
+    exports: [RouterModule],
+    declarations: [
+        AboutComponent
+    ]
 })
 export class AboutRoutingModule { }
