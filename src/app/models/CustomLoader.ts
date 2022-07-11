c9ca71457569a0ba389080e0b0532b36809b6062
ghchfolio <ghchfolio@gmail.com>
2022-07-11 00:05:51 -0500

    removed unused operator

--- src/app/models/CustomLoader.ts
+++ src/app/models/CustomLoader.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateLoader } from '@ngx-translate/core';
-import { forkJoin, Observable, of } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Resources } from '../models/Resources'
+import { Resources } from '../models/Resources';
 
 export class CustomLoader implements TranslateLoader {
 
@@ -10,9 +10,7 @@ export class CustomLoader implements TranslateLoader {
 
     // We supply getTranslation with our own custom loader (the default loader only loads one resource).
     // This will load multiple resources then join them into 1 json file.
-
     getTranslation(lang: string): Observable<any> {
-
         const header$ = this.http.get(Resources.headerPartial);
         const body$ = this.http.get(Resources.bodyPartial);
         const footer$ = this.http.get(Resources.footerPartial);
